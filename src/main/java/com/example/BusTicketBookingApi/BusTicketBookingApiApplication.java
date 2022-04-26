8a5dd028e46ae74b6731f9e3b63765f4a8492a56
saipratap99 <122005067@sastra.ac.in>
2022-04-26 21:44:55 +0530

    Initial commit

---
+++ src/main/java/com/example/BusTicketBookingApi/BusTicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusTicketBookingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BusTicketBookingApi/BusTicketBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusTicketBookingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
